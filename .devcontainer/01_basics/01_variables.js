7376948fc325b4461d07418d18a96ff47d67e2ca
Rajesh-joc <140595451+Rajesh-joc@users.noreply.github.com>
2023-10-07 07:01:35 +0000

    dataType

---
+++ .devcontainer/01_basics/01_variables.js
@@ -0,0 +1,13 @@
+const accountId = 145442
+let accountEmail = "joshi@google.com"
+var accountPassword = "12345"
+accontCity = "dehradun"
+accountEmail = "hc@123"
+accountPassword = "34523423"
+//accountId = 3   //with const keyword it is not allowed to make changes to constant words
+
+/*prefer not to use var 
+because of issue in block scope and functional scope
+*/
+console.log(accountId);
+console.table([accountId ,accountEmail , accountPassword , accontCity]);
---
+++ .devcontainer/01_basics/02_dataType.js
@@ -0,0 +1,25 @@
+"use strict"; // treat all JS code as newer version
+
+//alert(3+3)  // we are using nodejs , not browser
+
+console.log(3+3) 
+console.log("rajesh")
+ // code readability that should be high
+
+ // data types 
+ /* 
+ number = 2 to power 53
+ bigint 
+ string = ""
+ boolean = true/false
+ null = standalone value
+ undefined = 
+ symbol = when we talk about unique 
+
+ these are primitive data type 
+
+
+
+ */
+
+ console.log(typeof "3") // type of is used for defining the type of the data
