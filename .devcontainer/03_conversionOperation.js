cedfc34dc4b85982809a0fc3449f309aabb77841
Rajesh-joc <140595451+Rajesh-joc@users.noreply.github.com>
2023-10-08 06:20:53 +0000

    c

--- .devcontainer/03_conversionOperation.js
+++ .devcontainer/03_conversionOperation.js
@@ -15,3 +15,35 @@ console.log(valueInNumber);
 true => 1
 false => 0
 */
+
+//***********OPERATIONS******************** **********/
+
+let value = 3
+let negValue = -value
+console.log(negValue)
+
+console.log(2+2);
+console.log(2-2);
+console.log(2*2);
+console.log(2**2);
+console.log(2/2);
+
+
+
+
+
+let str1 ="hello"
+let str2 = "rajesh "
+let str3 = str1 + str2
+console.log(str3);
+
+//********tricky conversion when we use string first whole line behave like string  see the outputs for easy to understand*/
+console.log("1" + 2);
+console.log(1 + "2");
+console.log("1" + 2 + 3);
+console.log( 2+ 5+ "1" ); //check the outputs to see different behaviour 
+
+console.log(true);
+console.log(+true); // true+ not possible 
+
+
