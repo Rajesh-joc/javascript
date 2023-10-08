8ad11f98f31fe42d77beec8b0455394152552ae0
Rajesh-joc <140595451+Rajesh-joc@users.noreply.github.com>
2023-10-08 04:42:21 +0000

    conversion of data type

---
+++ .devcontainer/03_conversionOperation.js
@@ -0,0 +1,17 @@
+let score = "33abc"
+
+console.log(typeof score);
+console.log(typeof(score));
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+// when we convert some numbers into other form than there will be some results
+/*
+ 
+"33" => 33
+"33and" => NaN
+true => 1
+false => 0
+*/
