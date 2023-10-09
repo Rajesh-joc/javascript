c802598c0b094248f117faa60f76351767fc949e
Rajesh-joc <140595451+Rajesh-joc@users.noreply.github.com>
2023-10-09 18:23:16 +0000

    string

---
+++ .devcontainer/01_basics/05_String.js
@@ -0,0 +1,12 @@
+const name = "rajesh"
+const age = 23
+
+//concatitation
+console.log(name + age + "value");
+
+// new method for concatitation
+console.log(`hello my name is ${name} and my age is ${age}`);
+
+// one more method for declare string
+
+const gameName =  new String('fjafjisdjfidasjfi')
